47ad4b0c9ddd4c974c7fa6252e26fd8e7aaab82c
samburger <twentythirdnumber@gmail.com>
2015-02-23 19:57:59 -0500

    reverted master branch to last working commit

--- PropulsionIterator.py
+++ PropulsionIterator.py
@@ -5,82 +5,111 @@ Handles bipropellant and monopropellant systems.
 Does not currently handle blow-down propellant storage considerations.
 """
 import math
-from tkinter import DoubleVar, IntVar
+
+# Rocket Equation Inputs
+thruster_mass = 54       # kg, Mass of the thruster itself.
+isp = 340                # sec
+delta_v = 1470          # m/sec
+contingency_factor = 1.1
+ox_fuel_ratio = 1.41     # Ratio of bipropellant oxidizer to fuel, by mass.  0 if monopropellant.
+m_f_initial = 2500+381.86    # kg, All mass above this stage - the "first guess" for dry mass.
 
 
-class PropInputs:
-    def __init__(self):
-        self.thruster_mass = DoubleVar()         # kg, Mass of the thruster itself.
-        self.isp = DoubleVar()                   # sec
-        self.delta_v = DoubleVar()               # m/sec
-        self.contingency = DoubleVar()
-        self.ox_fuel_ratio = DoubleVar()         # Ratio of bipropellant oxidizer to fuel, by mass. 0 if monopropellant.
-        self.payload = DoubleVar()               # kg, All mass above this stage - the "first guess" for dry mass.
-        self.fuel_density = DoubleVar()          # kg/m^3
-        self.ox_density = DoubleVar()            # kg/m^3, set 1 if monopropellant
-        self.aspect_ratio = DoubleVar()
-        self.mass_factor = DoubleVar()
-        self.wall_density = DoubleVar()          # kg/m^3
-        self.wall_s_ult = DoubleVar()            # MPa
-        self.safety_factor = DoubleVar()
-        self.op_pressure = DoubleVar()           # MPa
-        self.prop_type = IntVar()                # 1 if monoprop, 2 if biprop
-        self.fuel_tank_shape = IntVar()          # 1 if spherical, 2 if cylindrical
-        self.ox_tank_shape = IntVar()            # 1 if spherical, 2 if cylindrical
-
-        def calculate(prop_inputs):
-            result = PropOutputs()
-            result.iterations = 0
-            m_f = prop_inputs.payload
-            m_f_last = 0
-
-            while abs(m_f - m_f_last) > 0.01:
-                # Fuel Calculations
-                m_p = m_f*(math.exp(prop_inputs.delta_v.get()/(prop_inputs.isp.get()*9.8))-1)*prop_inputs.contingency.get()
-                result.fuel_mass = m_p/(prop_inputs.ox_fuel_ratio.get()+1)
-                result.oxidizer_mass = m_p-result.fuel_mass
-                result.fuel_volume = result.fuel_mass/prop_inputs.fuel_density.get()
-                result.oxidizer_volume = result.oxidizer_mass/prop_inputs.ox_density.get()
-
-                # Tank Calculations
-                if prop_inputs.fuel_tank_shape == 1:
-                    result.fuel_tank_radius = (result.fuel_volume/(math.pi*4/3)) ** (1/3)
-                    thickness_fuel_sph = prop_inputs.safety_factor.get()*prop_inputs.op_pressure.get()*result.fuel_tank_radius/(2*prop_inputs.wall_s_ult.get())
-                    result.fuel_tank_mass = prop_inputs.mass_factor.get()*prop_inputs.wall_density.get()*(4/3*math.pi*((result.fuel_tank_radius+thickness_fuel_sph)**3-result.fuel_tank_radius**3))
-                elif prop_inputs.fuel_tank_shape == 2:
-                    result.fuel_tank_radius = (result.fuel_volume/(math.pi*(prop_inputs.aspect_ratioget()+4/3)) ) ** (1/3)
-                    thickness_fuel_cyl = prop_inputs.safety_factor.get()*2*prop_inputs.op_pressure.get()*result.fuel_tank_radius/prop_inputs.wall_s_ult.get()
-                    thickness_fuel_cyl_caps = thickness_fuel_cyl/2
-                    result.fuel_tank_mass = prop_inputs.mass_factor.get()*prop_inputs.wall_density.get()*math.pi*(4/3*((result.fuel_tank_radius+thickness_fuel_cyl_caps)**3-result.fuel_tank_radius**3)+prop_inputs.aspect_ratio.get()*result.fuel_tank_radius*(2*result.fuel_tank_radius*thickness_fuel_cyl+thickness_fuel_cyl**2))
-                if prop_inputs.oxidizer_tank_mass == 1:
-                    result.oxidizer_tank_radius = (result.oxidizer_volume/(math.pi*4/3)) ** (1/3)
-                    thickness_ox_sph = prop_inputs.safety_factor.get()*prop_inputs.op_pressure.get()*result.oxidizer_tank_radius/(2*prop_inputs.wall_s_ult.get())
-                    result.oxidizer_tank_mass = prop_inputs.mass_factor.get()*prop_inputs.wall_density.get()*(4/3*math.pi*((result.oxidizer_tank_radius+thickness_ox_sph)**3-result.oxidizer_tank_radius**3))
-                elif prop_inputs.oxidizer_tank_mass == 2:
-                    result.oxidizer_tank_radius = (result.oxidizer_volume/(math.pi*(prop_inputs.aspect_ratioget()+4/3)) ) ** (1/3)
-                    thickness_ox_cyl = prop_inputs.safety_factor.get()*2*prop_inputs.op_pressure.get()*result.oxidizer_tank_radius/prop_inputs.wall_s_ult.get()
-                    thickness_ox_cyl_caps = thickness_ox_cyl/2
-                    result.oxidizer_mass = prop_inputs.mass_factor.get()*prop_inputs.wall_density.get()*math.pi*(4/3*((result.oxidizer_tank_radius+thickness_ox_cyl_caps)**3-result.oxidizer_tank_radius**3)+prop_inputs.aspect_ratio.get()*result.oxidizer_tank_radius*(2*result.oxidizer_tank_radius*thickness_ox_cyl+thickness_ox_cyl**2))
-
-                m_f_last, m_f = m_f, prop_inputs.payload.get() + result.fuel_tank_mass + result.oxidizer_tank_mass + prop_inputs.thruster_mass.get()
-                result.iterations += 1
-
-            result.sys_dry_mass = prop_inputs.thruster_mass.get() + result.fuel_tank_mass + result.oxidizer_tank_mass
-            result.sys_wet_mass = result.sys_dry_mass + result.fuel_mass + result.oxidizer_mass
-            result.vehicle_wet_mass = m_f + result.fuel_mass + result.oxidizer_mass
-            return result
+# Prop Tank Sizing Inputs
+density_fuel = 1008.3    # kg/m^3
+density_ox = 2720      # kg/m^3, set 1 if monopropellant
+aspect_ratio = 3.5
+mass_factor = 1.25
+wall_density = 4430.0      # kg/m^3
+wall_s_ult = 900.0         # MPa
+safety_factor = 2.0
+op_pressure = 6.89      # MPa
 
 
-class PropOutputs:
-    def __init__(self):
-        self.iterations = 0
-        self.fuel_mass = 0
-        self.oxidizer_mass = 0
-        self.fuel_volume = 0
-        self.oxidizer_volume = 0
-        self.fuel_tank_mass = 0
-        self.oxidizer_tank_mass = 0
-        self.fuel_tank_radius = 0
-        self.sys_dry_mass = 0
-        self.sys_wet_mass = 0
-        self.vehicle_wet_mass = 0
+count_sph = 0
+count_cyl = 0
+
+
+# Spherical Tank
+
+m_f_sph = m_f_initial
+m_f_last_sph = 0
+
+while abs(m_f_sph - m_f_last_sph) > 0.01:
+    # Fuel Mass Calculations
+    m_i = m_f_sph*math.exp(delta_v/(isp*9.8))
+    m_p = m_f_sph*(math.exp(delta_v/(isp*9.8))-1)*contingency_factor
+    m_fuel = m_p/(ox_fuel_ratio+1)
+    m_ox = m_p-m_fuel
+
+
+    # Tank Mass Calculations
+    volume_fuel = m_fuel/density_fuel
+    volume_ox = m_ox/density_ox
+
+    radius_fuel_sph = ( volume_fuel/(math.pi*4/3) ) ** (1/3)
+    radius_ox_sph = ( volume_ox/(math.pi*4/3) ) ** (1/3)
+
+    thickness_fuel_sph = safety_factor*op_pressure*radius_fuel_sph/(2*wall_s_ult)
+    thickness_ox_sph = safety_factor*op_pressure*radius_ox_sph/(2*wall_s_ult)
+
+    tank_mass_fuel_sph = mass_factor*wall_density*(4/3*math.pi*((radius_fuel_sph+thickness_fuel_sph)**3-radius_fuel_sph**3))
+    tank_mass_ox_sph = mass_factor*wall_density*(4/3*math.pi*((radius_ox_sph+thickness_ox_sph)**3-radius_ox_sph**3))
+
+    m_f_last_sph = m_f_sph
+    m_f_sph = m_f_initial + tank_mass_fuel_sph + tank_mass_ox_sph + thruster_mass
+    count_sph += 1
+
+print("Spherical tank:")
+print("Converged after " + str(count_sph) + " iterations with difference of " + str(round(abs(m_f_sph - m_f_last_sph),4)) + "kg from previous iteration.")
+print("Fuel tank radius = " + str(round(radius_fuel_sph, 4)))
+print("Oxidizer tank radius = " + str(round(radius_ox_sph, 4)))
+print("System dry mass = " + str(round(tank_mass_fuel_sph + tank_mass_ox_sph + thruster_mass, 4)))
+print("System wet mass = " + str(round(tank_mass_fuel_sph + tank_mass_ox_sph + thruster_mass + m_fuel + m_ox, 4)))
+print("Fuel mass = " + str(round(m_fuel,4)))
+print("Oxidizer mass = " + str(round(m_ox,4)))
+print("Fuel tank mass = " + str(round(tank_mass_fuel_sph,4)))
+print("Oxidizer tank mass = " + str(round(tank_mass_ox_sph,4)))
+print("Stage dry mass = " + str(round(m_f_sph, 4)))
+print("Stage wet mass = " + str(round(m_f_sph + m_fuel + m_ox, 4)) + "\n")
+
+
+# Cylindrical Tank
+
+m_f_cyl = m_f_initial
+m_f_last_cyl = 0
+
+while abs(m_f_cyl - m_f_last_cyl) > 0.01:
+    # Fuel Mass Calculations
+    m_i = m_f_cyl*math.exp(delta_v/(isp*9.8))
+    m_p = m_f_cyl*(math.exp(delta_v/(isp*9.8))-1)*contingency_factor
+    m_fuel = m_p/(ox_fuel_ratio+1)
+    m_ox = m_p-m_fuel
+
+    # Tank Mass Calculations
+    volume_fuel = m_fuel/density_fuel
+    volume_ox = m_ox/density_ox
+
+    radius_fuel_cyl = ( volume_fuel/(math.pi*(aspect_ratio+4/3)) ) ** (1/3)
+    radius_ox_cyl = ( volume_ox/(math.pi*(aspect_ratio+4/3)) ) ** (1/3)
+
+    thickness_fuel_cyl = safety_factor*2*op_pressure*radius_fuel_cyl/wall_s_ult
+    thickness_ox_cyl = safety_factor*2*op_pressure*radius_ox_cyl/wall_s_ult
+    thickness_fuel_cyl_caps = thickness_fuel_cyl/2
+    thickness_ox_cyl_caps = thickness_ox_cyl/2
+
+    tank_mass_fuel_cyl = mass_factor*wall_density*math.pi*(4/3*((radius_fuel_cyl+thickness_fuel_cyl_caps)**3-radius_fuel_cyl**3)+aspect_ratio*radius_fuel_cyl*(2*radius_fuel_cyl*thickness_fuel_cyl+thickness_fuel_cyl**2))
+    tank_mass_ox_cyl = mass_factor*wall_density*math.pi*(4/3*((radius_ox_cyl+thickness_ox_cyl_caps)**3-radius_ox_cyl**3)+aspect_ratio*radius_ox_cyl*(2*radius_ox_cyl*thickness_ox_cyl+thickness_ox_cyl**2))
+
+    m_f_last_cyl = m_f_cyl
+    m_f_cyl = m_f_initial + tank_mass_fuel_cyl + tank_mass_ox_cyl + thruster_mass
+    count_cyl += 1
+
+print("Cylindrical tank:")
+print("Converged after " + str(count_cyl) + " iterations with difference of " + str(round(abs(m_f_cyl - m_f_last_cyl),4)) + "kg from previous iteration.")
+print("Fuel tank radius = " + str(round(radius_fuel_cyl, 4)))
+print("Oxidizer tank radius = " + str(round(radius_ox_cyl, 4)))
+print("System dry mass = " + str(round(tank_mass_fuel_cyl + tank_mass_ox_cyl + thruster_mass, 4)))
+print("System wet mass = " + str(round(tank_mass_fuel_cyl + tank_mass_ox_cyl + thruster_mass + m_fuel + m_ox, 4)))
+print("Stage dry mass = " + str(round(m_f_cyl, 4)))
+print("Stage wet mass = " + str(round(m_f_cyl + m_fuel + m_ox, 4)))
