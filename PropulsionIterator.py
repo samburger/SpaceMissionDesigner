6a7e1a473b0655fce6fc8fc5823bec214bd1561e
samburger <twentythirdnumber@gmail.com>
2015-02-23 21:22:22 -0500

    added results section

--- PropulsionIterator.py
+++ PropulsionIterator.py
@@ -28,59 +28,60 @@ class PropInputs:
         self.fuel_tank_shape = IntVar()          # 1 if spherical, 2 if cylindrical
         self.ox_tank_shape = IntVar()            # 1 if spherical, 2 if cylindrical
 
-        def calculate(prop_inputs):
-            result = PropOutputs()
-            result.iterations = 0
-            m_f = prop_inputs.payload
-            m_f_last = 0
+    def calculate(self):
+        result = PropOutputs()
+        result.iterations = IntVar()
+        m_f = self.payload.get()
+        m_f_last = 0
 
-            while abs(m_f - m_f_last) > 0.01:
-                # Fuel Calculations
-                m_p = m_f*(math.exp(prop_inputs.delta_v.get()/(prop_inputs.isp.get()*9.8))-1)*prop_inputs.contingency.get()
-                result.fuel_mass = m_p/(prop_inputs.ox_fuel_ratio.get()+1)
-                result.oxidizer_mass = m_p-result.fuel_mass
-                result.fuel_volume = result.fuel_mass/prop_inputs.fuel_density.get()
-                result.oxidizer_volume = result.oxidizer_mass/prop_inputs.ox_density.get()
+        while abs(m_f - m_f_last) > 0.01:
+            # Fuel Calculations
+            m_p = m_f*(math.exp(self.delta_v.get()/(self.isp.get()*9.8))-1)*self.contingency.get()
+            result.fuel_mass.set(m_p/(self.ox_fuel_ratio.get()+1))
+            result.oxidizer_mass.set(m_p-result.fuel_mass.get())
+            result.fuel_volume.set(result.fuel_mass.get()/self.fuel_density.get())
+            result.oxidizer_volume.set(result.oxidizer_mass.get()/self.ox_density.get())
 
-                # Tank Calculations
-                if prop_inputs.fuel_tank_shape == 1:
-                    result.fuel_tank_radius = (result.fuel_volume/(math.pi*4/3)) ** (1/3)
-                    thickness_fuel_sph = prop_inputs.safety_factor.get()*prop_inputs.op_pressure.get()*result.fuel_tank_radius/(2*prop_inputs.wall_s_ult.get())
-                    result.fuel_tank_mass = prop_inputs.mass_factor.get()*prop_inputs.wall_density.get()*(4/3*math.pi*((result.fuel_tank_radius+thickness_fuel_sph)**3-result.fuel_tank_radius**3))
-                elif prop_inputs.fuel_tank_shape == 2:
-                    result.fuel_tank_radius = (result.fuel_volume/(math.pi*(prop_inputs.aspect_ratioget()+4/3)) ) ** (1/3)
-                    thickness_fuel_cyl = prop_inputs.safety_factor.get()*2*prop_inputs.op_pressure.get()*result.fuel_tank_radius/prop_inputs.wall_s_ult.get()
-                    thickness_fuel_cyl_caps = thickness_fuel_cyl/2
-                    result.fuel_tank_mass = prop_inputs.mass_factor.get()*prop_inputs.wall_density.get()*math.pi*(4/3*((result.fuel_tank_radius+thickness_fuel_cyl_caps)**3-result.fuel_tank_radius**3)+prop_inputs.aspect_ratio.get()*result.fuel_tank_radius*(2*result.fuel_tank_radius*thickness_fuel_cyl+thickness_fuel_cyl**2))
-                if prop_inputs.oxidizer_tank_mass == 1:
-                    result.oxidizer_tank_radius = (result.oxidizer_volume/(math.pi*4/3)) ** (1/3)
-                    thickness_ox_sph = prop_inputs.safety_factor.get()*prop_inputs.op_pressure.get()*result.oxidizer_tank_radius/(2*prop_inputs.wall_s_ult.get())
-                    result.oxidizer_tank_mass = prop_inputs.mass_factor.get()*prop_inputs.wall_density.get()*(4/3*math.pi*((result.oxidizer_tank_radius+thickness_ox_sph)**3-result.oxidizer_tank_radius**3))
-                elif prop_inputs.oxidizer_tank_mass == 2:
-                    result.oxidizer_tank_radius = (result.oxidizer_volume/(math.pi*(prop_inputs.aspect_ratioget()+4/3)) ) ** (1/3)
-                    thickness_ox_cyl = prop_inputs.safety_factor.get()*2*prop_inputs.op_pressure.get()*result.oxidizer_tank_radius/prop_inputs.wall_s_ult.get()
-                    thickness_ox_cyl_caps = thickness_ox_cyl/2
-                    result.oxidizer_mass = prop_inputs.mass_factor.get()*prop_inputs.wall_density.get()*math.pi*(4/3*((result.oxidizer_tank_radius+thickness_ox_cyl_caps)**3-result.oxidizer_tank_radius**3)+prop_inputs.aspect_ratio.get()*result.oxidizer_tank_radius*(2*result.oxidizer_tank_radius*thickness_ox_cyl+thickness_ox_cyl**2))
+            # Tank Calculations
+            if self.fuel_tank_shape == 1:
+                result.fuel_tank_radius.set((result.fuel_volume.get()/(math.pi*4/3)) ** (1/3))
+                thickness_fuel_sph = self.safety_factor.get()*self.op_pressure.get()*result.fuel_tank_radius/(2*self.wall_s_ult.get())
+                result.fuel_tank_mass.set(self.mass_factor.get()*self.wall_density.get()*(4/3*math.pi*((result.fuel_tank_radius.get()+thickness_fuel_sph)**3-result.fuel_tank_radius.get()**3)))
+            elif self.fuel_tank_shape == 2:
+                result.fuel_tank_radius.set((result.fuel_volume.get()/(math.pi*(self.aspect_ratio.get()+4/3)) ) ** (1/3))
+                thickness_fuel_cyl = self.safety_factor.get()*2*self.op_pressure.get()*result.fuel_tank_radius.get()/self.wall_s_ult.get()
+                thickness_fuel_cyl_caps = thickness_fuel_cyl/2
+                result.fuel_tank_mass.set(self.mass_factor.get()*self.wall_density.get()*math.pi*(4/3*((result.fuel_tank_radius.get()+thickness_fuel_cyl_caps)**3-result.fuel_tank_radius.get()**3)+self.aspect_ratio.get()*result.fuel_tank_radius*(2*result.fuel_tank_radius.get()*thickness_fuel_cyl+thickness_fuel_cyl**2)))
+            if result.oxidizer_tank_mass == 1:
+                result.oxidizer_tank_radius.set((result.oxidizer_volume.get()/(math.pi*4/3)) ** (1/3))
+                thickness_ox_sph = self.safety_factor.get()*self.op_pressure.get()*result.oxidizer_tank_radius/(2*self.wall_s_ult.get())
+                result.oxidizer_tank_mass.set(self.mass_factor.get()*self.wall_density.get()*(4/3*math.pi*((result.oxidizer_tank_radius.get()+thickness_ox_sph)**3-result.oxidizer_tank_radius.get()**3)))
+            elif result.oxidizer_tank_mass == 2:
+                result.oxidizer_tank_radius.set((result.oxidizer_volume.get()/(math.pi*(self.aspect_ratio.get()+4/3)) ) ** (1/3))
+                thickness_ox_cyl = self.safety_factor.get()*2*self.op_pressure.get()*result.oxidizer_tank_radius.get()/self.wall_s_ult.get()
+                thickness_ox_cyl_caps = thickness_ox_cyl/2
+                result.oxidizer_mass.set(self.mass_factor.get()*self.wall_density.get()*math.pi*(4/3*((result.oxidizer_tank_radius.get()+thickness_ox_cyl_caps)**3-result.oxidizer_tank_radius.get()**3)+self.aspect_ratio.get()*result.oxidizer_tank_radius.get()*(2*result.oxidizer_tank_radius.get()*thickness_ox_cyl+thickness_ox_cyl**2)))
 
-                m_f_last, m_f = m_f, prop_inputs.payload.get() + result.fuel_tank_mass + result.oxidizer_tank_mass + prop_inputs.thruster_mass.get()
-                result.iterations += 1
+            m_f_last, m_f = m_f, self.payload.get() + result.fuel_tank_mass.get() + result.oxidizer_tank_mass.get() + self.thruster_mass.get()
+            result.iterations.set(result.iterations.get()+1)
 
-            result.sys_dry_mass = prop_inputs.thruster_mass.get() + result.fuel_tank_mass + result.oxidizer_tank_mass
-            result.sys_wet_mass = result.sys_dry_mass + result.fuel_mass + result.oxidizer_mass
-            result.vehicle_wet_mass = m_f + result.fuel_mass + result.oxidizer_mass
-            return result
+        result.sys_dry_mass.set(self.thruster_mass.get() + result.fuel_tank_mass.get() + result.oxidizer_tank_mass.get())
+        result.sys_wet_mass.set(result.sys_dry_mass.get() + result.fuel_mass.get() + result.oxidizer_mass.get())
+        result.vehicle_wet_mass.set(m_f + result.fuel_mass.get() + result.oxidizer_mass.get())
+
 
 
 class PropOutputs:
     def __init__(self):
-        self.iterations = 0
-        self.fuel_mass = 0
-        self.oxidizer_mass = 0
-        self.fuel_volume = 0
-        self.oxidizer_volume = 0
-        self.fuel_tank_mass = 0
-        self.oxidizer_tank_mass = 0
-        self.fuel_tank_radius = 0
-        self.sys_dry_mass = 0
-        self.sys_wet_mass = 0
-        self.vehicle_wet_mass = 0
+        self.iterations = IntVar()
+        self.fuel_mass = DoubleVar()
+        self.oxidizer_mass = DoubleVar()
+        self.fuel_volume = DoubleVar()
+        self.oxidizer_volume = DoubleVar()
+        self.fuel_tank_mass = DoubleVar()
+        self.oxidizer_tank_mass = DoubleVar()
+        self.fuel_tank_radius = DoubleVar()
+        self.oxidizer_tank_radius = DoubleVar()
+        self.sys_dry_mass = DoubleVar()
+        self.sys_wet_mass = DoubleVar()
+        self.vehicle_wet_mass = DoubleVar()
--- SpaceMissionDesigner.py
+++ SpaceMissionDesigner.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from tkinter import ttk
-from PropulsionIterator import PropInputs
+from PropulsionIterator import PropInputs, PropOutputs
 
 
 class Application:
@@ -8,15 +8,16 @@ class Application:
         self.master = master
         master.title("Space Mission Designer")
         self.inputs = PropInputs()
+        self.results = PropOutputs()
 
         self.mainframe = ttk.Frame(master)
         self.mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
         self.mainframe.columnconfigure(0, weight=1)
         self.mainframe.rowconfigure(0, weight=1)
-
+        # FIXME: Do all layout after defining elements.  Grid returns None, breaking label updating.
         # Option Selection
         self.option_frame = ttk.LabelFrame(self.mainframe, text="Options", padding=(5, 5, 5, 5))
-        self.option_frame.grid(column=2, row=1, sticky=(N, W, E, S))
+        self.option_frame.grid(column=1, row=1, sticky=(N, W, E, S))
 
         ttk.Label(self.option_frame, text="Propellant Type:").grid(column=1, row=1, sticky=E)
         Radiobutton(self.option_frame, text="Monopropellant", variable=self.inputs.prop_type, value=1).grid(column=2, row=1, sticky=W)
@@ -30,14 +31,20 @@ class Application:
 
         # Propulsion inputs
         self.prop_frame = ttk.LabelFrame(self.mainframe, text="Propulsion inputs", padding=(5, 5, 5, 5))
-        self.prop_frame.grid(column=2, row=2, sticky=(N, W, E, S))
+        self.prop_frame.grid(column=1, row=2, sticky=(N, W, E, S))
 
-        isp_entry = ttk.Entry(self.prop_frame, width=10, textvariable=self.inputs.isp).grid(column=2, row=1, sticky=(W, E))
-        delta_v_entry = ttk.Entry(self.prop_frame, width=10, textvariable=self.inputs.delta_v).grid(column=2, row=2, sticky=(W, E))
-        m_f_initial_entry = ttk.Entry(self.prop_frame, width=10, textvariable=self.inputs.payload).grid(column=2, row=3, sticky=(W, E))
-        contingency_entry = ttk.Entry(self.prop_frame, width=10, textvariable=self.inputs.contingency).grid(column=5, row=1, sticky=(W, E))
-        ox_fuel_ratio_entry = ttk.Entry(self.prop_frame, width=10, textvariable=self.inputs.ox_fuel_ratio).grid(column=5, row=2, sticky=(W, E))
-        thruster_mass_entry = ttk.Entry(self.prop_frame, width=10, textvariable=self.inputs.thruster_mass).grid(column=5, row=3, sticky=(W, E))
+        isp_entry = ttk.Entry(self.prop_frame, width=10, textvariable=self.inputs.isp)
+        isp_entry.grid(column=2, row=1, sticky=(W, E))
+        delta_v_entry = ttk.Entry(self.prop_frame, width=10, textvariable=self.inputs.delta_v)
+        delta_v_entry.grid(column=2, row=2, sticky=(W, E))
+        m_f_initial_entry = ttk.Entry(self.prop_frame, width=10, textvariable=self.inputs.payload)
+        m_f_initial_entry.grid(column=2, row=3, sticky=(W, E))
+        contingency_entry = ttk.Entry(self.prop_frame, width=10, textvariable=self.inputs.contingency)
+        contingency_entry.grid(column=5, row=1, sticky=(W, E))
+        ox_fuel_ratio_entry = ttk.Entry(self.prop_frame, width=10, textvariable=self.inputs.ox_fuel_ratio)
+        ox_fuel_ratio_entry.grid(column=5, row=2, sticky=(W, E))
+        thruster_mass_entry = ttk.Entry(self.prop_frame, width=10, textvariable=self.inputs.thruster_mass)
+        thruster_mass_entry.grid(column=5, row=3, sticky=(W, E))
 
         # Labels
         ttk.Label(self.prop_frame, text="Isp =").grid(column=1, row=1, sticky=E)
@@ -53,16 +60,24 @@ class Application:
 
         # Tank Sizing inputs
         self.tank_frame = ttk.LabelFrame(self.mainframe, text="Propellant Tank Sizing", padding=(5, 5, 5, 5))
-        self.tank_frame.grid(column=2, row=3, sticky=(N, W, E, S))
+        self.tank_frame.grid(column=1, row=3, sticky=(N, W, E, S))
 
-        fuel_density_entry = ttk.Entry(self.tank_frame, width=10, textvariable=self.inputs.fuel_density).grid(column=2, row=1, sticky=(W ,E))
-        ox_density_entry = ttk.Entry(self.tank_frame, width=10, textvariable=self.inputs.ox_density).grid(column=2, row=2, sticky=(W, E))
-        op_pressure_entry = ttk.Entry(self.tank_frame, width=10, textvariable=self.inputs.op_pressure).grid(column=2, row=3, sticky=(W, E))
-        aspect_ratio_entry = ttk.Entry(self.tank_frame, width=10, textvariable=self.inputs.aspect_ratio).grid(column=2, row=4, sticky=(W, E))
-        safety_factor_entry = ttk.Entry(self.tank_frame, width=10, textvariable=self.inputs.safety_factor).grid(column=5, row=1, sticky=(W, E))
-        mass_factor_entry = ttk.Entry(self.tank_frame, width=10, textvariable=self.inputs.mass_factor).grid(column=5, row=2, sticky=(W, E))
-        wall_density_entry = ttk.Entry(self.tank_frame, width=10, textvariable=self.inputs.wall_density).grid(column=5, row=3, sticky=(W, E))
-        wall_s_ult_entry = ttk.Entry(self.tank_frame, width=10, textvariable=self.inputs.wall_s_ult).grid(column=5, row=4, sticky=(W, E))
+        fuel_density_entry = ttk.Entry(self.tank_frame, width=10, textvariable=self.inputs.fuel_density)
+        fuel_density_entry.grid(column=2, row=1, sticky=(W ,E))
+        ox_density_entry = ttk.Entry(self.tank_frame, width=10, textvariable=self.inputs.ox_density)
+        ox_density_entry.grid(column=2, row=2, sticky=(W, E))
+        op_pressure_entry = ttk.Entry(self.tank_frame, width=10, textvariable=self.inputs.op_pressure)
+        op_pressure_entry.grid(column=2, row=3, sticky=(W, E))
+        aspect_ratio_entry = ttk.Entry(self.tank_frame, width=10, textvariable=self.inputs.aspect_ratio)
+        aspect_ratio_entry.grid(column=2, row=4, sticky=(W, E))
+        safety_factor_entry = ttk.Entry(self.tank_frame, width=10, textvariable=self.inputs.safety_factor)
+        safety_factor_entry.grid(column=5, row=1, sticky=(W, E))
+        mass_factor_entry = ttk.Entry(self.tank_frame, width=10, textvariable=self.inputs.mass_factor)
+        mass_factor_entry.grid(column=5, row=2, sticky=(W, E))
+        wall_density_entry = ttk.Entry(self.tank_frame, width=10, textvariable=self.inputs.wall_density)
+        wall_density_entry.grid(column=5, row=3, sticky=(W, E))
+        wall_s_ult_entry = ttk.Entry(self.tank_frame, width=10, textvariable=self.inputs.wall_s_ult)
+        wall_s_ult_entry.grid(column=5, row=4, sticky=(W, E))
 
         ttk.Label(self.tank_frame, text="Fuel Density =").grid(column=1, row=1, sticky=E)
         ttk.Label(self.tank_frame, text="kg/m^3").grid(column=3, row=1, sticky=W)
@@ -78,8 +93,61 @@ class Application:
         ttk.Label(self.tank_frame, text="Tank Material s_ult =").grid(column=4, row=4, sticky=E)
         ttk.Label(self.tank_frame, text="MPa").grid(column=6, row=4, sticky=W)
 
+        # Calculate Button
+        ttk.Button(self.mainframe, text="Calculate", command=self.calculate).grid(column=1, row=4, sticky=(N, W, E, S))
+
         # Results
         self.results_frame = ttk.LabelFrame(self.mainframe, text="Results")
+        self.results_frame.grid(column=1, row=5, sticky=(N, W, E, S))
+
+        # Prop section
+        self.results_prop = ttk.Frame(self.results_frame)
+        self.results_prop.grid(column=1, row=1, sticky=(N, W, E, S))
+        ttk.Label(self.results_prop, text="Fuel Mass: ").grid(column=1, row=1, sticky=E)
+        ttk.Label(self.results_prop, text="Fuel Volume: ").grid(column=1, row=2, sticky=E)
+        ttk.Label(self.results_prop, text="Oxidizer Mass: ").grid(column=4, row=1, sticky=E)
+        ttk.Label(self.results_prop, text="Oxidizer Volume: ").grid(column=4, row=2, sticky=E)
+        ttk.Label(self.results_prop, text="kg").grid(column=3, row=1, sticky=W)
+        ttk.Label(self.results_prop, text="m^3").grid(column=3, row=2, sticky=W)
+        ttk.Label(self.results_prop, text="kg").grid(column=6, row=1, sticky=W)
+        ttk.Label(self.results_prop, text="m^3").grid(column=6, row=2, sticky=W)
+        ttk.Label(self.results_prop, textvariable=self.results.fuel_mass).grid(column=2, row=1, sticky=(W, E))
+        ttk.Label(self.results_prop, textvariable=self.results.fuel_volume).grid(column=2, row=2, sticky=(W, E))
+        ttk.Label(self.results_prop, textvariable=self.results.oxidizer_mass).grid(column=5, row=1, sticky=(W, E))
+        ttk.Label(self.results_prop, textvariable=self.results.oxidizer_volume).grid(column=5, row=2, sticky=(W, E))
+
+        # Tank section
+        self.results_tank = ttk.Frame(self.results_frame)
+        self.results_tank.grid(column=1, row=2, sticky=(N, W, E, S))
+        ttk.Label(self.results_tank, text="Fuel Tank Mass: ").grid(column=1, row=1, sticky=E)
+        ttk.Label(self.results_tank, text="Fuel Tank Radius: ").grid(column=1, row=2, sticky=E)
+        ttk.Label(self.results_tank, text="Oxidizer Tank Mass: ").grid(column=4, row=1, sticky=E)
+        ttk.Label(self.results_tank, text="Oxidizer Tank Radius: ").grid(column=4, row=2, sticky=E)
+        ttk.Label(self.results_tank, text="kg").grid(column=3, row=1, sticky=W)
+        ttk.Label(self.results_tank, text="m").grid(column=3, row=2, sticky=W)
+        ttk.Label(self.results_tank, text="kg").grid(column=6, row=1, sticky=W)
+        ttk.Label(self.results_tank, text="m").grid(column=6, row=2, sticky=W)
+        ttk.Label(self.results_tank, textvariable=self.results.fuel_tank_mass).grid(column=2, row=1, sticky=(W, E))
+        ttk.Label(self.results_tank, textvariable=self.results.fuel_tank_radius).grid(column=2, row=2, sticky=(W, E))
+        ttk.Label(self.results_tank, textvariable=self.results.oxidizer_tank_mass).grid(column=5, row=1, sticky=(W, E))
+        ttk.Label(self.results_tank, textvariable=self.results.oxidizer_tank_radius).grid(column=5, row=2, sticky=(W, E))
+
+        # System section
+        self.results_system = ttk.Frame(self.results_frame)
+        self.results_system.grid(column=1, row=3, sticky=(N,W,E,S))
+        ttk.Label(self.results_system, text="Propulsion System Dry Mass: ").grid(column=1, row=1, sticky=E)
+        ttk.Label(self.results_system, text="Propulsion System Wet Mass: ").grid(column=1, row=2, sticky=E)
+        ttk.Label(self.results_system, text="Vehicle Wet Mass: ").grid(column=1, row=3, sticky=E)
+        ttk.Label(self.results_system, text="kg").grid(column=3, row=1, sticky=W)
+        ttk.Label(self.results_system, text="kg").grid(column=3, row=2, sticky=W)
+        ttk.Label(self.results_system, text="kg").grid(column=3, row=3, sticky=W)
+        ttk.Label(self.results_system, textvariable=self.results.sys_dry_mass).grid(column=2, row=1, sticky=(W, E))
+        ttk.Label(self.results_system, textvariable=self.results.sys_wet_mass).grid(column=2, row=2, sticky=(W, E))
+        ttk.Label(self.results_system, textvariable=self.results.vehicle_wet_mass).grid(column=2, row=3, sticky=(W, E))
+
+    def calculate(self):
+        self.results = self.inputs.calculate()
+        print(self.results.fuel_mass)
 
 root = Tk()
 main_gui = Application(root)
